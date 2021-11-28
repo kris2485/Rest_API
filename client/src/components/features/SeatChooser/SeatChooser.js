84c1493572a0d062dd8b71fa03eb3b609404191c
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-11-28 16:33:25 +0100

    Add free seats

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -7,9 +7,9 @@ import './SeatChooser.scss';
 class SeatChooser extends React.Component {
   componentDidMount() {
     const { loadSeats, loadSeatsData } = this.props;
+    loadSeats();
     this.socket = io.connect(process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8000');
     this.socket.on('seatsUpdated', (seats) => loadSeatsData(seats));
-    loadSeats();
   }
 
   isTaken = (seatId) => {
@@ -44,7 +44,7 @@ class SeatChooser extends React.Component {
 
   render() {
     const { prepareSeat } = this;
-    const { requests } = this.props;
+    const { requests, seats, chosenDay } = this.props;
 
     return (
       <div>
@@ -58,6 +58,7 @@ class SeatChooser extends React.Component {
         {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success && <div className='seats'>{[...Array(50)].map((x, i) => prepareSeat(i + 1))}</div>}
         {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending && <Progress animated color='primary' value={50} />}
         {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error && <Alert color='warning'>Couldn't load seats...</Alert>}
+        <h4>Free seats: {50 - seats.filter((item) => item.day === chosenDay).length} / 50</h4>
       </div>
     );
   }
